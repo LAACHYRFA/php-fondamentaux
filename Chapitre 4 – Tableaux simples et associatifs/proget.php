d1c666e77ccd251640e1a6c4b9ea88009b92297e
LAACHYRFA <laachyr.farah.solicode@gmail.com>
2026-01-08 12:00:14 +0100

    Add files via upload

---
+++ Chapitre 4 – Tableaux simples et associatifs/proget.php
@@ -0,0 +1,20 @@
+$animaux = ["cheval", "chien", "lapin"];
+echo "pemier animal :" . $animaux[0] . "<br>" ;
+
+foreach ($animaux as $animal) {
+
+ echo "animal : $animal <br>";
+}
+
+$voiture = [
+ "marque" => "honda",
+ "modele" =< "civic"
+"année" => 2022
+
+ ];
+
+ $fruits = [ "orange" , "avocado"];
+ array_push($fruits , "mangue")
+ unset($fruits[1]);
+
+ echo "nombre de fruits :" count($fruits);
