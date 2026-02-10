242b6768a5a29ae5e55ad64422e55cb82a16f328
LAACHYRFA <laachyr.farah.solicode@gmail.com>
2026-02-10 10:05:35 +0100

    Add files via upload

---
+++ CHAPITRE7/footer.php
@@ -0,0 +1,5 @@
+<footer>
+<hr>
+<p> &copy; 2025 - MY SITE PHP</p>
+
+</footer>
---
+++ CHAPITRE7/index.php
@@ -0,0 +1,9 @@
+<?php
+include 'header.php';
+?>
+<main>
+<h2>page d'accueil</h2>
+<p> contenu de la page principale</p>
+
+
+</main>
